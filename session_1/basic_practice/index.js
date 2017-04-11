d4a24652a540fdd408bd0e6c5ba54606a98922b1
kw7oe <choongkwern@hotmail.com>
2017-04-11 17:05:50 +0800

    Modify Basic Practice

--- session_1/basic_practice/index.js
+++ session_1/basic_practice/index.js
@@ -1,24 +1,46 @@
+// Declare a String variable
+var name = 
+
+// Declare a Number variable
+var age = 
+
+// Declare a Boolean variable
+
+
+// Display the name in Console
+console.log("My name is " + name + ".");
+console.log("I am " + age + " years old.");
+
+// If/Else Statement
+
+
+// Dispay Your favourite color here.
+
+
+// Do some maths here.
+var x = 
+var y = 
+
+var addition = 
+var subtraction = 
+var multiplication = 
+var division = 
+
+console.log("Result of " + x + " + " + y + ": " + addition);
+console.log("Result of " + x + " - " + y + ": " + subtraction);
+console.log("Result of " + x + " x " + y + ": " + multiplication);
+console.log("Result of " + x + " / " + y + ": " + division);
+
 // Declare an array of name.
 
-// E.g.
-// var arrayName = [
-//    "Item 1", 
-//    "Item 2"
-// ]
-
-
 // Output the second name
-// console.log() is the function you can used.
+
+// Change the second name
+
 
 // Create a function to output element.
-
-// E.g.
-// function function_name(parameters) {
-//    Your code block here.
-// }
 
 
 // Loop through the names and display it here.
 
 
-// Practice 2 
