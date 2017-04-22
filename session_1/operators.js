f6baedf2c36cafe993ca528ff2a123c5f54fd033
Eric <ericngu97@gmail.com>
2017-04-22 23:33:07 +0800

    Added instructions

---
+++ session_1/operators.js
@@ -0,0 +1,34 @@
+// console log the results
+// var a
+// var b
+
+// a + b
+// a - b
+// a * b
+// a / b
+// a % b
+// a++
+// a--
+// what are the differences between the group above and below?
+// a = b
+// a += b
+// a -= b
+// a *= b
+// a /= b
+// a %= b
+
+// comparison operators (notice the output)
+// a == b
+// a === b
+// a != b
+// a !== b
+// a > b
+// a < b
+// a >= b
+// a <= b
+// [condition] ? [true] : [false]
+
+// logical operators
+// a && b
+// a || b
+// a ! b
